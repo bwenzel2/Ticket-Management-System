8081f7fe4d69ab8657bbb42500902430e05310e3
Benjamin Wenzel <benwenzel@runtimeparadigm.com>
2018-02-23 17:52:01 -0500

    Began database setup and integration

--- TicketMaster/TicketMaster/models.py
+++ TicketMaster/TicketMaster/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 class Ticket(models.Model):
-	room = models.CharField(max_length=200)
-	created_date = models.DateTimeField(default=timezone.now)
+	OPEN = 'OPEN'
+	IN_PROGRESS = 'IN PROGRESS'
+	CLOSED = 'CLOSED'
+	STATUS_CHOICES = (
+		(OPEN, 'Open'),
+		(IN_PROGRESS, 'In Progress'),
+		(CLOSED, 'Closed'),
+	)
+	room = models.CharField(max_length=200, default='')
+	#created_date = models.DateTimeField(default=timezone.now)
+	#description = models.TextField(default='')
+	#status = models.CharField(max_length=11, choices=STATUS_CHOICES, default=OPEN)
+	
+	def create(self):
+		#creation_date = timezone.now()
+		#status = Ticket.objects.filter(status=Ticket.OPEN)
+		self.save();
 	
--- TicketMaster/TicketMaster/views.py
+++ TicketMaster/TicketMaster/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.views import login
 from django.shortcuts import redirect
+from .models import Ticket
 
 
 # Create your views here.
@@ -8,8 +9,9 @@ def custom_login(request):
 	return render(request, 'home.html', {})
 
 def home(request):
+	tickets = Ticket.objects.filter()
 	if request.user.is_authenticated:
-		return render(request, 'home.html')
+		return render(request, 'home.html', {'tickets': tickets})
 	else:
 		return redirect('login');
 
