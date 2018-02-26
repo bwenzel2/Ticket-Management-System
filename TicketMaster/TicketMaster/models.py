889a1fdea5b26d8619fadd10ae78f57a705fc235
Benjamin Wenzel <benwenzel@runtimeparadigm.com>
2018-02-25 23:29:18 -0500

    Improved ticket database schema, implemented first pass at home screen layout

--- TicketMaster/TicketMaster/models.py
+++ TicketMaster/TicketMaster/models.py
@@ -2,21 +2,20 @@ from django.db import models
 from django.utils import timezone
 
 class Ticket(models.Model):
-	OPEN = 'OPEN'
-	IN_PROGRESS = 'IN PROGRESS'
-	CLOSED = 'CLOSED'
+	
 	STATUS_CHOICES = (
-		(OPEN, 'Open'),
-		(IN_PROGRESS, 'In Progress'),
-		(CLOSED, 'Closed'),
-	)
+				(0,'Open'),
+				(1,'In Progress'),
+				(2,'Closed'),
+		  )
+	
 	room = models.CharField(max_length=200, default='')
-	#created_date = models.DateTimeField(default=timezone.now)
-	#description = models.TextField(default='')
-	#status = models.CharField(max_length=11, choices=STATUS_CHOICES, default=OPEN)
+	created_date = models.DateTimeField(default=timezone.now)
+	#description = models.TextField(default="This is the default description")
+	status = models.IntegerField(choices=STATUS_CHOICES, default=0)
 	
 	def create(self):
-		#creation_date = timezone.now()
+		creation_date = timezone.now()
 		#status = Ticket.objects.filter(status=Ticket.OPEN)
 		self.save();
 	
