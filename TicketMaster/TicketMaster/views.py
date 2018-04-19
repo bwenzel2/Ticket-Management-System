ab51eaaacada39e49b734c22a5389640a68e15c5
omar <ojawaad97@gmail.com>
2018-04-19 18:08:06 -0400

    trying tabs with mainbranch copy

--- TicketMaster/TicketMaster/views.py
+++ TicketMaster/TicketMaster/views.py
@@ -16,11 +16,14 @@ def custom_login(request):
 
 @login_required
 def home(request):
-	tickets = Ticket.objects.filter()
-	return render(request, 'home.html', {'tickets': tickets})
+	tickets_all = Ticket.objects.filter()
+	tickets_open = Ticket.objects.filter(status="0")
+	tickets_inprogress = Ticket.objects.filter(status="1")
+	tickets_closed = Ticket.objects.filter(status="2")
+	return render(request, 'home.html', {'tickets_all': tickets_all,'tickets_open': tickets_open,'tickets_inprogress': tickets_inprogress,'tickets_closed': tickets_closed})
 
-#creates a new ticket	
-@login_required	
+#creates a new ticket
+@login_required
 def new_ticket(request):
 	if request.method == 'POST':
 		desc = request.POST.get('description')
@@ -41,49 +44,49 @@ def new_ticket(request):
 @login_required
 def get_ticket_details(request):
 	ticket_id = request.GET.get('ticket_id')
-	
+
 	#find the ticket with that id, return it as a response
 	ticket = Ticket.objects.filter(id=ticket_id)
 	response = serializers.serialize("json", ticket)
 	return HttpResponse(response, content_type='application/json')
 
-		
-#creates a new update for a specified ticket id	
+
+#creates a new update for a specified ticket id
 @login_required
 def new_update(request):
 	if request.method == 'POST':
 		#get the POST paramenters
 		desc = request.POST.get('description')
 		ticket_id = request.POST.get('ticket_id')
-		
+
 		#get the ticket with the id specified in the POST request
 		t = Ticket.objects.get(id=ticket_id)
-		
+
 		#create a new ticket and store its id
-		newId = Update.objects.create(description=desc, ticket=t).id	
-		
+		newId = Update.objects.create(description=desc, ticket=t).id
+
 		#use the id of the new ticket to create a queryset to send back to the client
 		update = Update.objects.filter(id=newId)
 		response = serializers.serialize("json", update, use_natural_foreign_keys=True)
 		return HttpResponse(response, content_type='application/json')
 	else:
 		raise Http404()
-	
+
 #get all updates associcated with a particular ticket id, sorted most recent to least recent, return as a JSON Object
 @login_required
 def get_updates(request):
-	if request.method == 'GET':		
+	if request.method == 'GET':
 		ticket_id = request.GET.get('ticket_id')
 		#https://stackoverflow.com/questions/761352/django-queryset-order
 		updates = Update.objects.filter(ticket=ticket_id).order_by('-creation_date')
-		
+
 		#https://stackoverflow.com/questions/26373992/use-jsonresponse-to-serialize-a-queryset-in-django-1-7
 		response = serializers.serialize("json", updates)
 		return HttpResponse(response, content_type='application/json')
-		
+
 	else:
 		return redirect('login')
-		
+
 #used to assign tickets to the currently-logged-in user
 @login_required
 def assign_ticket(request):
@@ -93,11 +96,11 @@ def assign_ticket(request):
 	ticket.assigned_user = request.user
 	ticket.status = 1
 	ticket.save()	#any time you modify a database element in a view, you need to call save() to save the modifications to the database
-	
+
 	tickets = Ticket.objects.filter(id=ticket_id)
 	response = serializers.serialize("json", tickets, use_natural_foreign_keys=True)
 	return HttpResponse(response, content_type='application/json')
-	
+
 @login_required
 def close_ticket(request):
 	ticket_id = request.POST.get('ticket_id')
@@ -108,7 +111,7 @@ def close_ticket(request):
 	ticket.status = 2
 	ticket.solution = solution_text
 	ticket.save()	#any time you modify a database element in a view, you need to call save() to save the modifications to the database
-	
+
 	tickets = Ticket.objects.filter(id=ticket_id)
 	response = serializers.serialize("json", tickets, use_natural_foreign_keys=True)
 	return HttpResponse(response, content_type='application/json')
