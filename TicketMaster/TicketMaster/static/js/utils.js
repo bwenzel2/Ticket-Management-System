e7a5bc055931849423b35cbe05b53c69e7aa1ad9
Benjamin Wenzel <benwenzel@runtimeparadigm.com>
2018-04-22 15:00:15 -0400

    Code cleanup, assign default values to Requestor and Recipient fields rather than leaving them blank.

--- TicketMaster/TicketMaster/static/js/utils.js
+++ TicketMaster/TicketMaster/static/js/utils.js
@@ -22,19 +22,22 @@ tbody.onclick = function (e) {
             ticket_id: ticketid
         },
         success: function (result) {
-            //alert('Successful' + JSON.stringify(result));
-
+            
             //location
             var location = result[0].fields.location;
             $('#location_detail').val(location);
 
             //requestor
             var requestor = result[0].fields.requestor;
-            $('#requestor_detail').val(requestor);
+            if (requestor != "") {
+                $('#requestor_detail').val(requestor);
+            }
 
             //recipient
             var recipient = result[0].fields.recipient;
-            $('#recipient_detail').val(recipient);
+            if (recipient != "") {
+                $('#recipient_detail').val(recipient);
+            }
 
             //status
             var status_int = result[0].fields.status;
@@ -62,10 +65,9 @@ tbody.onclick = function (e) {
                 $('#addUpdate').hide();
                 $('#acceptTicketButton').hide();
                 $('#closeTicketButton').hide();
+                
+                //show solution
                 $('#solution').show();
-
-
-                //add solution to the detail view
             }
             $('#status_detail').val(status_string);
 
@@ -121,8 +123,7 @@ tbody.onclick = function (e) {
     $('#ticketDetailModal').modal('show');
 };
 
-
-function addUpdate() {
+function setupAjaxCSRF() {
     //Django CSRF setup for the POST request
     $.ajaxSetup({
         beforeSend: function (xhr, settings) {
@@ -147,6 +148,12 @@ function addUpdate() {
             }
         }
     });
+};
+
+
+function addUpdate() {
+    //Django CSRF setup for the POST request
+    setupAjaxCSRF();
 
     //send the AJAX POST request
     var ticketid = $('#ticketDetailTitle').text().split('#')[1];
@@ -162,9 +169,6 @@ function addUpdate() {
             // add the new alert at the top of the activity log
             var dateString = result[0].fields.creation_date;
             $('#detail_activity_log').prepend('<div class="list-group-item flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><small>' + result[0].fields.creator + ' on ' + dateString.substring(5, 7) + '/' + dateString.substring(8, 10) + '/' + dateString.substring(0, 4) + '</small></div>' + result[0].fields.description + '</div>'
-
-
-                //'<li class="list-group-item"><p style="font-size: 10px">' + result[0].fields.creator + " on " + dateString.substring(5, 7) + '/' + dateString.substring(8, 10) + '/' + dateString.substring(0, 4) + '</p><br><p>' + result[0].fields.description + '</p></li>'
             );
 
             $('#updateLog').show();
@@ -180,29 +184,7 @@ function addUpdate() {
 
 function assignTicket() {
     //Django CSRF setup for the POST request
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            function getCookie(name) {
-                var cookieValue = null;
-                if (document.cookie && document.cookie != '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = jQuery.trim(cookies[i]);
-                        // Does this cookie string begin with the name we want?
-                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                            break;
-                        }
-                    }
-                }
-                return cookieValue;
-            }
-            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-                // Only send the token to relative URLs i.e. locally.
-                xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
-            }
-        }
-    });
+    setupAjaxCSRF();
 
     //send the AJAX POST request
     var ticketid = $('#ticketDetailTitle').text().split('#')[1];
@@ -213,26 +195,6 @@ function assignTicket() {
             ticket_id: ticketid
         },
         success: function (result) {
-            //alert('Success' + result[0].fields.status);
-
-            //reset update text input field value
-            // var status_int = result[0].fields.status;
-            // var status_string;
-            // if (status_int == "0") {
-            //     status_string = "Open";
-            // } else if (status_int == "1") {
-            //     status_string = "In Progress";
-            // } else if (status_int == "2") {
-            //     status_string = "Closed";
-            // }
-            // $('#status_detail').val(status_string);
-            //
-            //
-            // //update the status in the ticket table
-            // var tableRow = $("td").filter(function () {
-            //     return $(this).text() == ticketid;
-            // }).closest("tr");
-            // tableRow.find("td:eq(1)").text("In Progress");
             
             var assigned_user = result[0].fields.assigned_user[0];
             if (assigned_user != null) {
@@ -258,29 +220,8 @@ function assignTicket() {
 
 function closeTicket() {
     //Django CSRF setup for the POST request
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            function getCookie(name) {
-                var cookieValue = null;
-                if (document.cookie && document.cookie != '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = jQuery.trim(cookies[i]);
-                        // Does this cookie string begin with the name we want?
-                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                            break;
-                        }
-                    }
-                }
-                return cookieValue;
-            }
-            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-                // Only send the token to relative URLs i.e. locally.
-                xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
-            }
-        }
-    });
+    setupAjaxCSRF();
+    
     var ticketid = $('#ticketDetailTitle').text().split('#')[1];
     $.ajax({
         url: '/close_ticket/',
@@ -290,12 +231,6 @@ function closeTicket() {
             solution: $('#solution_text').val()
         },
         success: function (result) {
-            //update the status in the ticket table
-            // var tableRow = $("td").filter(function () {
-            //     return $(this).text() == ticketid;
-            // }).closest("tr");
-            // tableRow.find("td:eq(1)").text("Closed");
-            //reload to home page
             window.location = "{%url 'home'%}";
         },
         error: function (status) {
