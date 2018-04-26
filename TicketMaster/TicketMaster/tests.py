b2e719b75e08a589a5a9992c9f22585df543faab
Benjamin Wenzel <benwenzel@runtimeparadigm.com>
2018-04-25 22:59:19 -0400

    Initial testing setup for Ticket model

--- TicketMaster/TicketMaster/tests.py
+++ TicketMaster/TicketMaster/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Ticket
+
+
+class TickeModelTests(TestCase):
+	
+	@classmethod
+	def setUpTestData(cls):
+		#Set up non-modified objects used by all test methods
+		desc = "This is a sample description"
+		Ticket.objects.create(description=desc, urgency=2, requestor="Sample Requestor", recipient="Sample Recipient", location="Sample Location")
+	
+	def testTicketDescription(self):
+		new_ticket = Ticket.objects.get(id=1)
+		self.assertEqual(new_ticket.description, "This is a sample description")
